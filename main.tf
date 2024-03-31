f860897e74dbadcdda9561d5dd9bcec03d7694f3
marriani <160234538+marriani@users.noreply.github.com>
2024-03-31 11:23:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami                     = "ami-007020fd9c84e18c7"
+  instance_type           = "t2.micro"
+  subnet_id               = subnet-07a8a03de128c955e
+  key _name               = ani1
+  tags = {
+    Name = "tf-example"
+  }
+}
